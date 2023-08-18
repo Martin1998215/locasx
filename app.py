1ac78e803076be1f4df97b7b85c3af7c155c3614
Martin Sichibeya <sichibeyam@gmail.com>
2023-08-18 11:19:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -963,7 +963,6 @@ if txt:
     final_response = response.split(delimiter)[-1].strip()
     res_word = len(re.findall(r'\w+', final_response))
     user_text = st.chat_message("user")
-    # user_text.session_state.past.append(txt)
     user_text.write(txt)
 
     if res_word < 3:

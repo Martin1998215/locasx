d9ba9f706de93b19e7c9ae9631837655bec64eec
Martin Sichibeya <sichibeyam@gmail.com>
2023-12-05 15:17:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -865,17 +865,18 @@ st.markdown("<h5 style='text-align: center; color: blue;'>NOTE: Always specify t
 st.markdown('''
 <div style='margin: 0; padding: 0; box-sizing: border-box;'>
 <div style='border: 0.3px solid light-gray; border-radius: 10%;'>
-<p style='color:blue;'><b>Access Menus for Restaurants eg</b><br>
+<p><b>Access Menus for Restaurants eg</b><br>
 <i>- Show me a menu for flavours? or Bravo Cafe or Sweet & Salty Cafe? (Lodges inclusive)</i></p>
 </div>
 <div>
-<p><b>Access Room rates or Conference or restaurant menus for Lodges e.g</b></p>
-<p><i>- Room rates for Livingstone Lodge? Chapa Classic Lodge? and More...</i></p>
+<p><b>Access Room rates or Conference or restaurant menus for Lodges e.g</b><br>
+<i>- Room rates for Livingstone Lodge? Chapa Classic Lodge? and More...</i></p>
 </div>
 <div>
-<p><b>Plan Trips or Date outings at your favourite Restaurants e.g</b></p>
-<p><i>- Make me a budget from bravo cafe within K200 for the following: 4 cold beverages, a large pizza and 2 con ice creams. also compare for kubu cafe and flavours</i></p>
-<p><i>- I will be travelling to livingstone. recommend for me some cheap accommodation and how much they cost</i></p>
+<p><b>Plan Trips or Date outings at your favourite Restaurants e.g</b><br>
+<i>- Make me a budget from bravo cafe within K200 for the following: 
+4 cold beverages, a large pizza and 2 con ice creams. also compare for kubu cafe and flavours</i><br>
+<i>- I will be travelling to livingstone. recommend for me some cheap accommodation and how much they cost</i></p>
 </div>
 </div>
 ''', unsafe_allow_html=True)

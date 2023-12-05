96f46b72d8c118f8063619e1aa5d1d8da9484914
Martin Sichibeya <sichibeyam@gmail.com>
2023-12-05 14:35:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -858,8 +858,8 @@ st.sidebar.write("---")
 st.sidebar.markdown("<h5 style='text-align: center; color: black;'>Copyrights © Quest2Query 2023</h5>", unsafe_allow_html=True)
 st.sidebar.markdown("<h5 style='text-align: center; color: blue;'>Powered By LocasAI</h5>", unsafe_allow_html=True)   
 
-st.markdown("<h2 style='text-align: center; color: gray;'>Quest2Query</h2>", unsafe_allow_html=True)
-st.markdown("<h3 style='text-align: center; color: lightgray;'>Some Suggested Questions...</h3>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center; color: gray;'>Quest2Query</h3>", unsafe_allow_html=True)
+# st.markdown("<h3 style='text-align: center; color: lightgray;'>Some Suggested Questions...</h3>", unsafe_allow_html=True)
 st.markdown("<h5 style='text-align: center; color: blue;'>NOTE: Always specify the name of the restaurant or lodge you are asking about.</h5>", unsafe_allow_html=True)
 
 st.write('''

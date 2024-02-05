b1bff0b283e41bf4d8bb2aae94a103c4776facb8
Martin Sichibeya <sichibeyam@gmail.com>
2024-02-05 18:04:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2159,7 +2159,7 @@ with for_you:
     "Sweet & Salty Cafe"))
 
 
-    text = st.text_input("Ask about the services- Menus [menu for platter or pizza or beverages...], Plan dates or food recommendations within certain budgets, room rates, Photo galleries, contact info etc ",
+    text = st.text_input("Search for specific menu items for the service [Prices for Pizza, Platters, Burgers, Beverages and more] Plan dates or food recommendations within certain budgets, room rates, Photo galleries, contact info etc ",
     max_chars=300)
 
     # examples: Services offered? Show me a menu? Room rates? Contact info? recommend food within certain budgets? Plan date meals within certain budgets? or Photo links? Google maps link?
